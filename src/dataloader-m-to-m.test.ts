e40f480c6263260a6c35023cb0d8b302a8533d3a
Graham Tackley <gtackley@brave.com>
2024-09-16 10:37:25 +0100

    dataloader error many-to-many
    example showing error resturned when attempting to use dataloader on a many to many relationship

---
+++ src/dataloader-m-to-m.test.ts
@@ -0,0 +1,66 @@
+import { Collection, Entity, ManyToMany, MikroORM, PrimaryKey, Property } from '@mikro-orm/sqlite';
+
+@Entity()
+export class Country {
+  @PrimaryKey()
+  code!: string;
+
+  @Property()
+  name!: string;
+
+  constructor(code: string, name: string) { 
+    this.code = code;
+    this.name = name;
+  }
+}
+
+
+@Entity()
+class User {
+
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  name: string;
+
+  @ManyToMany(() => Country)
+  countries = new Collection<Country>(this);
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+}
+
+let orm: MikroORM;
+
+beforeAll(async () => {
+  orm = await MikroORM.init({
+    dbName: ':memory:',
+    entities: [User],
+    debug: ['query', 'query-params'],
+    allowGlobalContext: true, // only for testing
+  });
+  await orm.schema.refreshDatabase();
+});
+
+afterAll(async () => {
+  await orm.close(true);
+});
+
+test('basic CRUD example', async () => {
+  orm.em.create(User, {
+    name: 'Foo', countries: [new Country("US", "United States")]
+   });
+  await orm.em.flush();
+  orm.em.clear();
+
+  const reloaded = await orm.em.findOneOrFail(User, { name: 'Foo' });
+
+  // not loaded, as expected
+  expect(reloaded.countries.isInitialized()).toBe(false);
+
+  await reloaded.countries.load({dataloader: true});
+
+});
